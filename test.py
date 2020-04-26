1a20ea8c51fcafe7495b5c8423d8f9af712c77dc
Alper Ekmekci <31824731+Spawnfile@users.noreply.github.com>
2020-04-26 21:41:33 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(490, 320)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(40, 190, 171, 41))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(260, 190, 171, 41))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(150, 50, 151, 41))
+        self.label.setObjectName("label")
+
+        self.pushButton.clicked.connect(self.write)
+        self.pushButton_2.clicked.connect(self.erase)
+        
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "PushButton"))
+        self.pushButton_2.setText(_translate("Form", "PushButton"))
+        self.label.setText(_translate("Form", "TextLabel"))
+
+    def write(self):
+        self.label.setText("Hello!")
+
+    def erase(self):
+        self.label.clear()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
